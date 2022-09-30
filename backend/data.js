616e9ea452b7c5f39b884f3bca7ca45b9ac9c644
CadeWeiskopf <weiskopfcd@gmail.com>
2022-09-30 11:18:12 -0400

    init commit

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Smiley T-Shirt',
+            slug: 'smiley-t-shirt',
+            category: 'T-shirts',
+            image: '/images/smileyshirt.jpg',
+            price: 10,
+            inStock: 59,
+            brand: 'Test Brand',
+            rating: 4.5,
+            numReviews: 3,
+            description: 'Really funk t-shirt with a cool smiley graphic!'
+        },
+        {
+            name: 'Frowney T-Shirt',
+            slug: 'frowney-t-shirt',
+            category: 'T-shirts',
+            image: '/images/frowneyshirt.jpg',
+            price: 70,
+            inStock: 1000,
+            brand: 'Test Brand',
+            rating: 5,
+            numReviews: 78,
+            description: 'Turn it upside down!'
+        },
+        {
+            name: 'Red Sox Hat',
+            slug: 'red-sox-hat',
+            category: 'Hats',
+            image: '/images/redsoxhat.jpg',
+            price: 88,
+            inStock: 2,
+            brand: 'Test Brand',
+            rating: 4,
+            numReviews: 99,
+            description: 'Go sox!'
+        }
+    ]
+}
+
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,40 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <a href="/">cade.csg</a>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>cade.csg</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
+
       <main>
-        products
+        <Container>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
       </main>
+
+      <footer>
+        <div className='text-center'>All rights reserved. -Cade Weiskopf @ CSG</div>
+      </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
