051ffe325ff06e60a3c0718dbc7feafa01202bf6
CadeWeiskopf <weiskopfcd@gmail.com>
2022-09-30 09:44:08 -0400

    First commit - created base app with banner link

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">cade.csg</a>
+      </header>
+      <main>
+        products
+      </main>
+    </div>
+  );
+}
+
+export default App;
