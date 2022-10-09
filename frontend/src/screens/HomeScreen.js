8ffa116c7ef7069ee58c7faf3b981eb3da9ecb40
CadeWeiskopf <weiskopfcd@gmail.com>
2022-10-08 22:57:40 -0400

    building cart -small format adjustments

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -54,11 +54,13 @@ function HomeScreen() {
                         <div>{error}</div>
                     ) : (
                         <Row>
-                        {products.map(product => (
-                            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-                                <Product product={product}></Product>
-                            </Col>
-                        ))}
+                        {
+                            products.map(product => (
+                                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                    <Product product={product}></Product>
+                                </Col>
+                            ))
+                        }
                         </Row>
                     )   
                 }
