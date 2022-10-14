18664d2ab750fc5c7c28ecb9baf049d69d44bcda
CadeWeiskopf <weiskopfcd@gmail.com>
2022-10-13 20:34:22 -0400

    style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
         <Navbar bg="dark" variant="dark">
           <Container>
             <LinkContainer to='/'>
-              <Navbar.Brand><img className='home-logo' src='/images/red_bolt_1.png' alt='cade.csg'></img>cade.csg</Navbar.Brand>
+              <Navbar.Brand><img className='home-logo' src='/images/red_bolt_1.png' alt='cade.csg'></img></Navbar.Brand>
             </LinkContainer>
             <Nav className="me-auto">
               <Link to="/cart" className='nav-link'>
@@ -64,7 +64,9 @@ function App() {
                   </NavDropdown>
                 ) : (
                   <Link className="nav-link" to="/signin">
-                    Sign In
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
                   </Link>
                 )
               }
